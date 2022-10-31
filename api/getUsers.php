37c272560c13e3ea22eaca5552c86f7589346f8f
EnderReal <enderreal1@gmail.com>
2022-10-31 07:48:11 +0200

    repair getUsers METHOD POST

--- api/getUsers.php
+++ api/getUsers.php
@@ -11,7 +11,7 @@ header("Access-Control-Allow-Methods: *");
 $method = $_SERVER['REQUEST_METHOD'];
 
 //NU STIU DACA MERGE AI GRIJA CAMARADE
-if ($method) {
+if ($method == "POST") {
 
     $data = json_decode(file_get_contents('php://input'));
 
