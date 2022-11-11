e5579c94a0a9ac2ae3f2aea2f52724ee49658b1b
EnderReal <enderreal1@gmail.com>
2022-11-11 21:55:22 +0200

    move full to heroku

--- core/init.php
+++ core/init.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: *");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 //SI MAI MULT KAMIKAZE
 // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
